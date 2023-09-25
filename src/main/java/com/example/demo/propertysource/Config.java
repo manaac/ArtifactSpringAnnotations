a7f9dc26c4bb2d29895b76bc5e0a57f1b642f23b
Manoj Kumar Pacha <manojkumarpacha@Manojs-MacBook-Pro.local>
2023-09-24 22:05:46 -0400

    @PropertySources demo

--- src/main/java/com/example/demo/propertysource/Config.java
+++ src/main/java/com/example/demo/propertysource/Config.java
@@ -2,9 +2,14 @@ package com.example.demo.propertysource;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @Configuration
-@PropertySource("classpath:mail.properties")
-@PropertySource("classpath:message.properties")
+//@PropertySource("classpath:mail.properties")
+//@PropertySource("classpath:message.properties")
+@PropertySources({
+        @PropertySource("classpath:mail.properties"),
+        @PropertySource("classpath:message.properties")
+})
 public class Config {
 }
